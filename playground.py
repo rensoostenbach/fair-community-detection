702ad782e89d6214e4b3be745a44200e30c37b79
rensoostenbach <rensoostenbach@hotmail.com>
2022-10-24 12:34:17 +0200

    Update playground

--- playground.py
+++ playground.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from metrics.ground_truth import purity, inverse_purity
 from metrics.own_metric import calculate_fairness_metrics
-from utils import draw_graph, small_large_communities, plot_fairness
+from utils import draw_graph, small_large_communities, plot_fairness, classify_graph
 
 
 # The below plot shows that EMD is most often much higher than the other two
@@ -16,28 +16,37 @@ from utils import draw_graph, small_large_communities, plot_fairness
 
 # In the (rare) case that all metrics are quite low: We can really say this was a fair classification
 
-n = 1000
-tau1 = 3
-tau2 = 1.5
-mu = 0.3
-seeds = list(range(20))
+N = 1000
+TAU1 = 3
+TAU2 = 1.5
+MU = 0.3
+SEEDS = list(range(20))
+PERCENTILE = 75
+
+fairness_graphs = []
+
+for seed in SEEDS:
+
+    try:
+        G = LFR_benchmark_graph(
+            n=N, tau1=TAU1, tau2=TAU2, mu=MU, min_degree=4, min_community=25, seed=seed
+        )
+        print(f"Graph created with seed: {seed}")
+        fairness_graphs.append(classify_graph(G=G, percentile=PERCENTILE))
+    except nx.ExceededMaxIterations:
+        print(f"Failed to create graph with seed: {seed}")
+        continue
+
+size_fairness_graphs = [x[0] for x in fairness_graphs if x[0] is not None]
+size_seeds = [i for i in range(len(fairness_graphs)) if fairness_graphs[i][0] != None]
+density_fairness_graphs = [x[1] for x in fairness_graphs if x[1] is not None]
+density_seeds = [i for i in range(len(fairness_graphs)) if fairness_graphs[i][1] != None]
+
 emd = []
 f1 = []
 acc = []
 
-for seed in seeds:
-
-    try:
-        G = LFR_benchmark_graph(
-            n=n, tau1=tau1, tau2=tau2, mu=mu, min_degree=4, min_community=25, seed=seed
-        )
-        print(f"Graph created with seed: {seed}")
-    except nx.ExceededMaxIterations:
-        emd.append(np.nan)
-        f1.append(np.nan)
-        acc.append(np.nan)
-        continue
-
+for G in size_fairness_graphs:
     G.remove_edges_from(nx.selfloop_edges(G))
 
     communities = {frozenset(G.nodes[v]["community"]) for v in G}
@@ -58,8 +67,8 @@ for seed in seeds:
         G=G,
         gt_communities=gt_communities,
         pred_communities=pred_coms.communities,
-        fairness_type="small_large",
-        size_percentile=75,
+        fairness_type="size",
+        percentile=PERCENTILE
     )
 
     emd.append(emd_fairness_score)
@@ -70,10 +79,56 @@ plot_fairness(
     emd=emd,
     f1=f1,
     acc=acc,
-    x_axis=seeds,
+    x_axis=size_seeds,
     xlabel="Seed chosen",
     title="Fairness score different seed \n(lines don't say anything but do show how they are correlated)",
 )
+print(np.array(emd)/np.array(f1))
+print(np.array(emd)/np.array(acc))
+
+emd = []
+f1 = []
+acc = []
+
+for G in density_fairness_graphs:
+    G.remove_edges_from(nx.selfloop_edges(G))
+
+    communities = {frozenset(G.nodes[v]["community"]) for v in G}
+    gt_communities = list(communities)
+
+    # CD part
+    pred_coms = algorithms.louvain(g_original=G, randomize=0)
+
+    #  Not important drawing stuff, just for myself
+    # pos = nx.spring_layout(G)  # compute graph layout
+    # draw_graph(G, pos=pos, communities=communities)
+
+    (
+        emd_fairness_score,
+        f1_fairness_score,
+        accuracy_fairness_score,
+    ) = calculate_fairness_metrics(
+        G=G,
+        gt_communities=gt_communities,
+        pred_communities=pred_coms.communities,
+        fairness_type="density",
+        percentile=PERCENTILE
+    )
+
+    emd.append(emd_fairness_score)
+    f1.append(f1_fairness_score)
+    acc.append(accuracy_fairness_score)
+
+plot_fairness(
+    emd=emd,
+    f1=f1,
+    acc=acc,
+    x_axis=density_seeds,
+    xlabel="Seed chosen",
+    title="Fairness score different seed \n(lines don't say anything but do show how they are correlated)",
+)
+print(np.array(emd)/np.array(f1))
+print(np.array(emd)/np.array(acc))
 
 # print(f"Purity: {purity(pred_coms=pred_coms.communities, real_coms=gt_communities)}")
 # print(
