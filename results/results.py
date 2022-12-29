4a1b569fea1064dcc9f524209ede148299bf4d2f
rensoostenbach <rensoostenbach@hotmail.com>
2022-12-29 15:51:41 +0100

    Write code towards final results plots

--- results/results.py
+++ results/results.py
@@ -1,37 +1,102 @@
 import pickle
 import os
-from utils import scatterplot_fairness
+from utils import scatterplot_fairness, match_result_to_underlying_method
 
 
-results = []
-results_directory = "DBLP"
-for root, dirs, files in os.walk(results_directory):
-    for file in files:
-        if file.endswith(".pickle"):
-            results.append(os.path.join(root, file))
+OPTIMIZATION = [
+    "louvain",
+    "leiden",
+    "cpm",
+    "significance_communities",
+    "surprise_communities",
+    "eigenvector",
+    "greedy_modularity",
+    "paris",
+    "pycombo",
+    "rb_pots",
+    "rber_pots"
+]
+PROPAGATION = [
+    "label_propagation",
+    "agdl",
+    "async_fluid",
+    "girvan_newman"
+]
+SPECTRAL = [
+    "kcut",
+    "r_spectral_clustering-vanilla",
+    "r_spectral_clustering-regularized_with_kmeans",
+    "r_spectral_clustering-sklearn_spectral_embedding",
+    "r_spectral_clustering-sklearn_kmeans",
+    "spectral",
+    "threshold_clustering",
+]
+BETWEENNESS = [
+    None  # Not using either of the two methods, should remove it here and in my thesis
+]
+DYNAMICS = ["infomap", "spinglass", "walktrap"]
+MATRIX = ["markov_clustering", "chinesewhispers"]
+REPRESENTATIONAL = ["Node2Vec", "der", "gemsec", "ricci_community"]
+OTHER = ["sbm_dl", "sbm_dl_nested", "belief", "edmot", "em", "ga", "gdmp2", "scd"]
 
-
-size_fairness_scores = {}
-size_evaluation_scores = {}
-density_fairness_scores = {}
-density_evaluation_scores = {}
-for result in results:
-    filename_splitted = os.path.basename(result).split("-")
-    with open(result, "rb") as pickled_results:
-        pickled_scores = pickle.load(pickled_results)
-        eval(f"{filename_splitted[0]}_{filename_splitted[2]}_scores")[
-            filename_splitted[1]
-        ] = pickled_scores
-
-# Now we make scatterplots of accuracy vs fairness
+# TODO: Perhaps change Matrix into something else
 for fairness_type in ["density", "size"]:
-    for evaluation_metric in ["ARI", "VI", "ARI_m", "Purity_m"]:
-        for fairness_metric in ["EMD", "F1", "FCC"]:
-            scatterplot_fairness(
-                fairness_scores=eval(f"{fairness_type}_fairness_scores"),
-                evaluation_scores=eval(f"{fairness_type}_evaluation_scores"),
-                fairness_metric=fairness_metric,
-                evaluation_metric=evaluation_metric,
-                title=f"{evaluation_metric} vs {fairness_metric} {fairness_type} Fairness",
-                filename=f"{results_directory}/Scatterplot_{fairness_type}_{evaluation_metric}_{fairness_metric}",
-            )
+    ALL_UNDERLYING_FAIRNESS = [
+        [{key: None for key in OPTIMIZATION}, "Optimization"],
+        [{key: None for key in PROPAGATION}, "Label propagation"],
+        [{key: None for key in SPECTRAL}, "Spectral properties"],
+        [{key: None for key in DYNAMICS}, "Dynamics"],
+        [{key: None for key in MATRIX}, "Matrix"],
+        [{key: None for key in REPRESENTATIONAL}, "Representationanl learning"],
+        [{key: None for key in OTHER}, "Other"],
+    ]
+    ALL_UNDERLYING_EVALUATION = [
+        [{key: None for key in OPTIMIZATION}, "Optimization"],
+        [{key: None for key in PROPAGATION}, "Label propagation"],
+        [{key: None for key in SPECTRAL}, "Spectral properties"],
+        [{key: None for key in DYNAMICS}, "Dynamics"],
+        [{key: None for key in MATRIX}, "Matrix"],
+        [{key: None for key in REPRESENTATIONAL}, "Representationanl learning"],
+        [{key: None for key in OTHER}, "Other"],
+    ]
+
+    results = []
+    results_directory = "Comparison test 1 dataset"
+    for root, _, files in os.walk(results_directory):
+        for file in files:
+            filename_splitted = os.path.basename(file).split("-")
+            fairness_type_file = filename_splitted[0]
+            if file.endswith(".pickle") and fairness_type_file == fairness_type:
+                method = filename_splitted[1]
+                score_type = filename_splitted[2]
+
+                with open(f"{root}/{file}", "rb") as pickled_results:
+                    pickled_scores = pickle.load(pickled_results)
+
+                if score_type == "fairness":
+                    ALL_UNDERLYING_FAIRNESS = match_result_to_underlying_method(
+                        cd_method=method,
+                        scores=pickled_scores,
+                        underlying_methods=ALL_UNDERLYING_FAIRNESS,
+                    )
+                else:  # evaluation
+                    ALL_UNDERLYING_EVALUATION = match_result_to_underlying_method(
+                        cd_method=method,
+                        scores=pickled_scores,
+                        underlying_methods=ALL_UNDERLYING_EVALUATION,
+                    )
+
+    # TODO: Change code below
+    for idx, scores in enumerate(zip(ALL_UNDERLYING_FAIRNESS, ALL_UNDERLYING_EVALUATION)):
+        for evaluation_metric in ["ARI", "VI", "ARI_m", "Purity_m"]:
+            for fairness_metric in ["EMD", "F1", "FCC"]:
+                scatterplot_fairness(
+                    fairness_scores=ALL_UNDERLYING_FAIRNESS[idx][0],
+                    evaluation_scores=ALL_UNDERLYING_EVALUATION[idx][0],
+                    fairness_metric=fairness_metric,
+                    evaluation_metric=evaluation_metric,
+                    title=f"{evaluation_metric} vs {fairness_metric} {fairness_type} Fairness"
+                          f" for underlying method {ALL_UNDERLYING_FAIRNESS[idx][1]}",
+                    filename=f"{results_directory}/Scatterplot_{fairness_type}_{evaluation_metric}_"
+                             f"{fairness_metric}_{ALL_UNDERLYING_FAIRNESS[idx][1]}",
+                )
---
+++ results/utils.py
@@ -0,0 +1,83 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def scatterplot_fairness(
+    fairness_scores: dict,
+    evaluation_scores: dict,
+    fairness_metric: str,
+    evaluation_metric: str,
+    title: str,
+    filename: str,
+):
+    """
+
+    :param fairness_scores: Dictionary containing fairness scores per method and fairness type
+    :param evaluation_scores: Dictionary containing accuracy values per method and accuracy type
+    :param fairness_metric: String indicating fairness type: EMD, F1, FCC
+    :param evaluation_metric: String indicating evaluation metrics: ARI, VI, ARI_m, Purity_m
+    :param filename: Filename of plot that will be saved
+    :return: Matplotlib plot
+    """
+    fairness_metrics = {"EMD": 0, "F1": 1, "FCC": 2}
+    evaluation_metrics = {"ARI": 0, "VI": 1, "ARI_m": 2, "Purity_m": 3}
+
+    for method, scores in fairness_scores.items():
+        if (
+            len(scores[fairness_metrics[fairness_metric]]) > 1
+        ):  # Not a real-world dataset, thus LFR datasets
+            fairness_score = [
+                x[fairness_metrics[fairness_metric]]
+                for x in scores
+                if x[fairness_metrics[fairness_metric]] is not None
+            ]
+            evaluation_score_list = [x for x in evaluation_scores[method]][
+                evaluation_metrics[evaluation_metric]
+            ]
+            eval_score = [x.score for x in evaluation_score_list if x is not None]
+
+        else:  # Real-world dataset
+            fairness_score = scores[fairness_metrics[fairness_metric]]
+            evaluation_score_list = [x for x in evaluation_scores[method]][
+                evaluation_metrics[evaluation_metric]
+            ]
+            eval_score = [x.score for x in evaluation_score_list if x is not None]
+
+        plt.errorbar(
+            x=np.mean(fairness_score),
+            y=np.mean(eval_score),
+            xerr=np.std(fairness_score),
+            yerr=np.std(eval_score),
+            label=f"{method}",
+            fmt="o",
+        )
+
+    plt.legend(bbox_to_anchor=(1.04, 1), loc="upper left")
+    plt.xlabel(f"Average Fairness score of type {fairness_metric}")
+    plt.ylabel(f"Accuracy of type {evaluation_metric}")
+    plt.title(title)
+    plt.xlim(0, 1)
+    if evaluation_metric != "VI":
+        plt.ylim(0, 1)
+    else:  # Variation of information, need to set different bound than 1.
+        max_vi = 0
+        for score in evaluation_scores.values():
+            matchingresult_per_method = score[1]
+            vi_per_method = [
+                x.score for x in matchingresult_per_method if x is not None
+            ]
+            for vi in vi_per_method:
+                if vi > max_vi:
+                    max_vi = vi
+        plt.ylim(0, max_vi)
+    plt.savefig(f"plots/{filename}.png", bbox_inches="tight")
+    plt.close()  # Use plot.show() if we want to show it
+
+
+def match_result_to_underlying_method(
+    cd_method: str, scores: tuple, underlying_methods: list
+):
+    for underlying_method in underlying_methods:
+        if cd_method in underlying_method[0].keys():
+            underlying_method[0][cd_method] = scores
+            return underlying_methods
