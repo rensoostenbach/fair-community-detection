8b2e86dbb2922be1b6b84f6dc4e389a95085d5c8
rensoostenbach <rensoostenbach@hotmail.com>
2022-12-29 12:43:14 +0100

    Improve comarison cd algs
    Add metrics, fix for spectral_clustering

--- comparison_cd_algs.py
+++ comparison_cd_algs.py
@@ -11,6 +11,7 @@ from cdlib.evaluation import adjusted_rand_index, variation_of_information
 from cdlib import NodeClustering
 
 from metrics.own_metric import calculate_fairness_metrics
+from metrics.ground_truth import modified_ari, modified_f_measure
 from utils import transform_sklearn_labels_to_communities
 
 """
@@ -36,9 +37,9 @@ CD_METHODS = {
     async_fluid: [None],
     infomap: None,
     walktrap: None,
-    girvan_newman: [
-        3
-    ],  # Took 20 minutes for ONE dataset! Very slow, not sure if I should include it
+    # girvan_newman: [
+    #     3
+    # ],  # Took 20 minutes for ONE dataset! Very slow, not sure if I should include it
     em: [None],
     # scan: [0.7, 3],
     gdmp2: None,
@@ -53,8 +54,8 @@ CD_METHODS = {
     ga: None,
     belief: None,
     threshold_clustering: None,
-    # mod_m: {"query_node": None},  # TODO: Need to find out what to do with this
-    # mod_r: {"query_node": None},  # TODO: Need to find out what to do with this
+    # mod_m: {"query_node": None},  # Only gives the community of the query node. Not sure if thats interesting, perhaps
+    # mod_r: {"query_node": None},  # I should write code to query all nodes individually, and then combine the results
     # head_tail: None,
     kcut: None,
     gemsec: None,
@@ -64,22 +65,17 @@ CD_METHODS = {
     ricci_community: None,
     spectral: None,
     # mcode: None,
-    # r_spectral_clustering: {
-    #     "n_clusters": None,
-    #     "method": [
-    #         "vanilla",
-    #         "regularized_with_kmeans",
-    #         "sklearn_spectral_embedding",
-    #         "sklearn_kmeans",
-    #     ],  # TODO: Take into account that we need to do 4 runs, one for each different method value
-    # },
+    "r_spectral_clustering-vanilla": None,
+    "r_spectral_clustering-regularized_with_kmeans": None,
+    "r_spectral_clustering-sklearn_spectral_embedding": None,
+    "r_spectral_clustering-sklearn_kmeans": None,
     Node2Vec: None,
 }
 FAIRNESS_TYPES = ["size", "density"]
-PERCENTILE = 75
+PERCENTILE = {"size": 75, "density": 50}
 CONFIGS = list(product(CD_METHODS.keys(), FAIRNESS_TYPES))
 
-ray.init(num_cpus=8)
+ray.init()
 
 
 @ray.remote
@@ -87,17 +83,25 @@ def evaluate_method(config):
     cd_method = config[0]
     fairness_type = config[1]
 
-    print(
-        f"{datetime.now()}: Starting with {cd_method.__name__} for fairness type {fairness_type}"
-    )
+    try:
+        print(
+            f"{datetime.now()}: Starting with {cd_method.__name__} for fairness type {fairness_type}"
+        )
+    except AttributeError:  # happens for r_spectral_clustering
+        print(
+            f"{datetime.now()}: Starting with {cd_method} for fairness type {fairness_type}"
+        )
+
     with open(f"data/labeled/lfr/{fairness_type}_seeds.txt") as seeds_file:
         seeds = [line.rstrip() for line in seeds_file][0]
 
     emd = []
     f1 = []
-    acc = []
+    fcc = []
     ari = []
     vi = []
+    ari_m = []
+    F_measure_m = []
     for seed in seeds:
         with open(
             f"data/labeled/lfr/{fairness_type}_graph_{seed}.pickle", "rb"
@@ -111,7 +115,16 @@ def evaluate_method(config):
 
             # CD part
             try:
-                if cd_method.__name__ == "Node2Vec":
+                if cd_method in [
+                    "r_spectral_clustering-vanilla",
+                    "r_spectral_clustering-regularized_with_kmeans",
+                    "r_spectral_clustering-sklearn_spectral_embedding",
+                    "r_spectral_clustering-sklearn_kmeans",
+                ]:
+                    method = cd_method.split("-")[1]  # Grab the element after the - icon
+                    cd_method_function = r_spectral_clustering
+                    pred_coms = cd_method_function(g_original=G, method=method)
+                elif cd_method.__name__ == "Node2Vec":
                     g_emb = Node2Vec(G)
                     mdl = g_emb.fit()
                     emb_df = pd.DataFrame(
@@ -124,6 +137,7 @@ def evaluate_method(config):
                         labels=kmeans.labels_
                     )
                     pred_coms = NodeClustering(communities=pred_coms, graph=G)
+
                 else:
                     if CD_METHODS[cd_method] is None:
                         pred_coms = cd_method(g_original=G)
@@ -132,10 +146,11 @@ def evaluate_method(config):
                             if value is None:
                                 CD_METHODS[cd_method][idx] = len(gt_communities)
                         pred_coms = cd_method(G, *CD_METHODS[cd_method])
-            except Exception:  # In case of convergence error or something else
+            except Exception as e:  # In case of convergence error or something else
+                print(repr(e))
                 emd.append(None)
                 f1.append(None)
-                acc.append(None)
+                fcc.append(None)
                 ari.append(None)
                 vi.append(None)
                 continue
@@ -143,39 +158,59 @@ def evaluate_method(config):
             (
                 emd_fairness_score,
                 f1_fairness_score,
-                accuracy_fairness_score,
+                fcc_fairness_score,
             ) = calculate_fairness_metrics(
                 G=G,
                 gt_communities=gt_communities,
                 pred_communities=pred_coms.communities,
-                fairness_type="size",
-                percentile=PERCENTILE,
+                fairness_type=fairness_type,
+                percentile=PERCENTILE[fairness_type],
             )
 
             emd.append(emd_fairness_score)
             f1.append(f1_fairness_score)
-            acc.append(accuracy_fairness_score)
-            ari.append(adjusted_rand_index(cdlib_communities, pred_coms))
-            vi.append(variation_of_information(cdlib_communities, pred_coms))
+            fcc.append(fcc_fairness_score)
 
-    fairness_scores = (emd, f1, acc)
-    evaluation_scores = (ari, vi)
+            ari.append(adjusted_rand_index(first_partition=cdlib_communities, second_partition=pred_coms))
+            vi.append(variation_of_information(first_partition=cdlib_communities, second_partition=pred_coms))
+            ari_m.append(modified_ari(pred_coms=pred_coms.communities, real_coms=gt_communities, G=G))
+            F_measure_m.append(modified_f_measure(pred_coms=pred_coms.communities, real_coms=gt_communities, G=G))
 
-    with open(
-        f"results/Comparison test 1 dataset/{fairness_type}-{cd_method.__name__}-fairness-scores.pickle",
-        "wb",
-    ) as handle:
-        pickle.dump(fairness_scores, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    fairness_scores = (emd, f1, fcc)
+    evaluation_scores = (ari, vi, ari_m, F_measure_m)
 
-    with open(
-        f"results/Comparison test 1 dataset/{fairness_type}-{cd_method.__name__}-evaluation-scores.pickle",
-        "wb",
-    ) as handle:
-        pickle.dump(evaluation_scores, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    try:
+        with open(
+            f"results/Initial comparison/{fairness_type}-{cd_method.__name__}-fairness-scores.pickle",
+            "wb",
+        ) as handle:
+            pickle.dump(fairness_scores, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-    print(
-        f"{datetime.now()}: Done with {cd_method.__name__} for fairness type {fairness_type}"
-    )
+        with open(
+            f"results/Initial comparison/{fairness_type}-{cd_method.__name__}-evaluation-scores.pickle",
+            "wb",
+        ) as handle:
+            pickle.dump(evaluation_scores, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+        print(
+            f"{datetime.now()}: Done with {cd_method.__name__} for fairness type {fairness_type}"
+        )
+    except AttributeError:  # Happens for r_spectral_clustering
+        with open(
+            f"results/Initial comparison/{fairness_type}-{cd_method}-fairness-scores.pickle",
+            "wb",
+        ) as handle:
+            pickle.dump(fairness_scores, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+        with open(
+            f"results/Initial comparison/{fairness_type}-{cd_method}-evaluation-scores.pickle",
+            "wb",
+        ) as handle:
+            pickle.dump(evaluation_scores, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+        print(
+            f"{datetime.now()}: Done with {cd_method} for fairness type {fairness_type}"
+        )
 
 
 if __name__ == "__main__":
