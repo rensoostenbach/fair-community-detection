b292e6bc3faa0c361bd4db426712a129ca53232f
rensoostenbach <rensoostenbach@hotmail.com>
2023-02-07 20:52:39 +0100

    Slightly change livejournal script

--- data/labeled/real_world/LiveJournal/livejournal.py
+++ data/labeled/real_world/LiveJournal/livejournal.py
@@ -1,7 +1,11 @@
 from data.utils import process_stanford_graph
 
-process_stanford_graph(
-    edgelist_file="com-lj.ungraph.txt",
-    community_file="com-lj.top5000.cmty.txt",
-    outname="LiveJournal_top5000",
-)
+outname = "LiveJournal_top5000_overlapping_processed"
+
+if __name__ == '__main__':
+
+    process_stanford_graph(
+        edgelist_file="com-lj.ungraph.txt",
+        community_file="com-lj.top5000.cmty.txt",
+        outname=outname,
+    )
