1f3b509caedda763afe5eec083ad9c539997655a
rensoostenbach <rensoostenbach@hotmail.com>
2023-02-13 16:48:28 +0100

    Joblib instead of ray

--- real_world_datasets_comparison.py
+++ real_world_datasets_comparison.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from itertools import product
 from sklearn.cluster import KMeans
 from cdlib.algorithms import *
-import ray
+from joblib import Parallel, delayed
 from node2vec import Node2Vec
 from cdlib.evaluation import adjusted_rand_index, variation_of_information
 from cdlib import NodeClustering
@@ -22,9 +22,10 @@ This file will be used to compare all CD Methods on their fairness and accuracy.
 # head_tail: Process finished with exit code -1073741571 (0xC00000FD)
 
 CD_METHODS = {
+    spinglass: None,
     louvain: None,
     leiden: None,
-    rb_pots: None,
+    # rb_pots: None,
     rber_pots: None,
     cpm: None,
     significance_communities: None,
@@ -38,25 +39,24 @@ CD_METHODS = {
     # girvan_newman: [
     #     3
     # ],  # Took 20 minutes for ONE dataset! Very slow, not sure if I should include it
-    em: [None],
+    # em: [None],
     # scan: [0.7, 3],
-    gdmp2: None,
-    spinglass: None,
+    # gdmp2: None,
+    # spinglass: None,
     eigenvector: None,
     agdl: [None, 4],
     sbm_dl: None,
     sbm_dl_nested: None,
     markov_clustering: None,
-    edmot: None,
+    # edmot: None,
     chinesewhispers: None,
-    ga: None,
+    # ga: None,
     belief: None,
-    threshold_clustering: None,
     # mod_m: {"query_node": None},  # Only gives the community of the query node. Not sure if thats interesting, perhaps
     # mod_r: {"query_node": None},  # I should write code to query all nodes individually, and then combine the results
     # head_tail: None,
     kcut: None,
-    gemsec: None,
+    # gemsec: None,
     scd: None,
     pycombo: None,
     paris: None,
@@ -73,10 +73,10 @@ FAIRNESS_TYPES = ["size", "density"]
 PERCENTILE = {"size": 75, "density": 50}
 CONFIGS = list(product(CD_METHODS.keys(), FAIRNESS_TYPES))
 
-ray.init()
+# TODO: Can make this file work on all the LiveJournal datasets instead of one.
+#  Roughly same approach as comparison_cd_algs.py
 
 
-@ray.remote
 def evaluate_method(config):
     cd_method = config[0]
     fairness_type = config[1]
@@ -95,10 +95,10 @@ def evaluate_method(config):
     fcc = []
     ari = []
     vi = []
-    ari_m = []
-    F_measure_m = []
+    # F_measure_m = []
     with open(
-        f"data/labeled/real_world/LiveJournal/LiveJournal_top5000_overlapping_processed.pickle", "rb"
+        f"data/labeled/real_world/LiveJournal/LiveJournal_top5000_overlapping_processed_0.pickle",
+        "rb",
     ) as graph_file:
         G = pickle.load(graph_file)
         G.remove_edges_from(nx.selfloop_edges(G))
@@ -106,6 +106,8 @@ def evaluate_method(config):
         communities = {frozenset(G.nodes[v]["community"]) for v in G}
         gt_communities = list(communities)
         cdlib_communities = NodeClustering(communities=gt_communities, graph=G)
+
+        print(f"Read graph and starting predictions for method {cd_method}")
 
         # CD part
         try:
@@ -131,7 +133,12 @@ def evaluate_method(config):
                     labels=kmeans.labels_
                 )
                 pred_coms = NodeClustering(communities=pred_coms, graph=G)
-
+            elif cd_method.__name__ == "cpm":  # Specific case in terms of parameters
+                pred_coms = cd_method(g_original=G, resolution_parameter=0.01)
+            elif (
+                cd_method.__name__ == "spectral"
+            ):  # Specific case in terms of parameters
+                pred_coms = cd_method(g_original=G, kmax=int(len(G.nodes) / 10))
             else:
                 if CD_METHODS[cd_method] is None:
                     pred_coms = cd_method(g_original=G)
@@ -141,12 +148,14 @@ def evaluate_method(config):
                             CD_METHODS[cd_method][idx] = len(gt_communities)
                     pred_coms = cd_method(G, *CD_METHODS[cd_method])
         except Exception as e:  # In case of convergence error or something else
-            print(repr(e))
+            print(f"Exception {repr(e)} for method {cd_method}")
             emd.append(None)
             f1.append(None)
             fcc.append(None)
             ari.append(None)
             vi.append(None)
+
+        print(f"prediction completed for method {cd_method}")
 
         (
             emd_fairness_score,
@@ -160,17 +169,26 @@ def evaluate_method(config):
             percentile=PERCENTILE[fairness_type],
         )
 
+        print(f"fairness metrics calculated for method {cd_method}")
+
         emd.append(emd_fairness_score)
         f1.append(f1_fairness_score)
         fcc.append(fcc_fairness_score)
 
-        ari.append(adjusted_rand_index(first_partition=cdlib_communities, second_partition=pred_coms))
-        vi.append(variation_of_information(first_partition=cdlib_communities, second_partition=pred_coms))
-        ari_m.append(modified_ari(pred_coms=pred_coms.communities, real_coms=gt_communities, G=G))
-        F_measure_m.append(modified_f_measure(pred_coms=pred_coms.communities, real_coms=gt_communities, G=G))
+        ari.append(
+            adjusted_rand_index(
+                first_partition=cdlib_communities, second_partition=pred_coms
+            )
+        )
+        vi.append(
+            variation_of_information(
+                first_partition=cdlib_communities, second_partition=pred_coms
+            )
+        )
+        # F_measure_m.append(modified_f_measure(pred_coms=pred_coms.communities, real_coms=gt_communities, G=G))
 
     fairness_scores = (emd, f1, fcc)
-    evaluation_scores = (ari, vi, ari_m, F_measure_m)
+    evaluation_scores = (ari, vi)
 
     try:
         with open(
@@ -208,5 +226,8 @@ def evaluate_method(config):
 
 if __name__ == "__main__":
     for configuration in CONFIGS:
-        # evaluate_method(configuration)
-        evaluate_method.remote(configuration)
+        evaluate_method(configuration)
+
+    # Parallel(n_jobs=-1)(
+    #     delayed(evaluate_method)(configuration) for configuration in CONFIGS
+    # )
